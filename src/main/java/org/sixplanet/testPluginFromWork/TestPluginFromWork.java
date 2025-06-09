8a7138f86afb4195d731da42d58780473b388a0a
sixplanet312 <vulturej372@gmail.com>
2025-06-09 16:50:07 -0700

    Had to move to projects folder manually. Try cloning repository at home and testing on the server. Maven build ran and converted to .jar file.

---
+++ src/main/java/org/sixplanet/testPluginFromWork/TestPluginFromWork.java
@@ -0,0 +1,20 @@
+package org.sixplanet.testPluginFromWork;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestPluginFromWork extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+        // Test plugin file from work.
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
